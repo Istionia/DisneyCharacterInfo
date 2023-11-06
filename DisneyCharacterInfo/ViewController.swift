69007dab1cf8b09c132ce5aa1e4070aa4b16b339
Istionia <timmoyoong678@protonmail.com>
2023-11-06 14:40:36 +0800

    Initial Commit

---
+++ DisneyCharacterInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisneyCharacterInfo
+//
+//  Created by Timothy Yoong Jie Gen on 06/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
